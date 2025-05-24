3ad64d312d4a928b53eba2ecd2533ee1061a5e29
algernon19 <seleyjanos0@gmail.com>
2025-05-24 15:04:57 +0200

    autentikáció fix

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -2,21 +2,19 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private loggedIn = false;
-
   login(username: string, password: string): boolean {
     if (username === 'admin' && password === 'admin') {
-      this.loggedIn = true;
+      localStorage.setItem('loggedIn', 'true');
       return true;
     }
     return false;
   }
 
   logout() {
-    this.loggedIn = false;
+    localStorage.removeItem('loggedIn');
   }
 
   isLoggedIn(): boolean {
-    return this.loggedIn;
+    return localStorage.getItem('loggedIn') === 'true';
   }
 }
