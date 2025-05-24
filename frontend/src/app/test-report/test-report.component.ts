2bc9bbe5c87fcba712a8f29474b469c5dbd3725f
TerhesBalazs <terhes.balazs@tigra.hu>
2025-05-24 13:56:27 +0200

    teszt riportáló felület

---
+++ frontend/src/app/test-report/test-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-report',
+  templateUrl: './test-report.component.html',
+  styleUrls: ['./test-report.component.css']
+})
+export class TestReportComponent {
+  download(type: string) {
+    alert('Letöltés: ' + type.toUpperCase());
+  }
+}
