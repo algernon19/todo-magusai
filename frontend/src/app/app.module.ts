c9e20ecf80c2d7acebb5c9fa391f1290c6193182
Tóth Ákos <takos2070@gmail.com>
2025-05-24 16:29:09 +0200

    Rossz merge

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
-
-=======
->>>>>>> 7c270da (Login, register)
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
-=======
+
 import { RegisterComponent } from './register/register.component';
->>>>>>> 7c270da (Login, register)
 import { TestCaseListComponent } from './test-case-list/test-case-list.component';
 import { NewTestCaseComponent } from './new-test-case/new-test-case.component';
 import { TestReportComponent } from './test-report/test-report.component';
