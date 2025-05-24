8800327bd8a3b16d27833436374e1855db55c6f2
algernon19 <seleyjanos0@gmail.com>
2025-05-24 15:43:05 +0200

    dummy törlés fix

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { DummyHomeComponent } from './dummy-home/dummy-home.component';
 import { TestCaseListComponent } from './test-case-list/test-case-list.component';
 import { NewTestCaseComponent } from './new-test-case/new-test-case.component';
 import { TestReportComponent } from './test-report/test-report.component';
