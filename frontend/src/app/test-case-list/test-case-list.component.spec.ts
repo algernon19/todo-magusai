74e9a03c44229418a37380a15c4d00dbeb6ad022
TerhesBalazs <terhes.balazs@tigra.hu>
2025-05-24 13:56:03 +0200

    teszt eset listázó felület

---
+++ frontend/src/app/test-case-list/test-case-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TestCaseListComponent } from './test-case-list.component';
+
+describe('TestCaseListComponent', () => {
+  let component: TestCaseListComponent;
+  let fixture: ComponentFixture<TestCaseListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TestCaseListComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TestCaseListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/test-case-list/test-case-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-test-case-list',
+  templateUrl: './test-case-list.component.html',
+  styleUrls: ['./test-case-list.component.css']
+})
+export class TestCaseListComponent {
+  testCases = [
+    { title: 'Demo test', priority: 'High', status: 'Draft', createdAt: '14 May, 2021' },
+    { title: 'Application to test', priority: 'High', status: 'Draft', createdAt: '4 July, 2021' },
+    { title: 'Test Soft', priority: 'Medium', status: 'Low', createdAt: '11 Jan, 2021' },
+    { title: 'Soire test', priority: 'Draft', status: 'Tester', createdAt: '15 May, 2021' }
+  ];
+
+  constructor(private router: Router) {}
+
+  navigateToNew() {
+    this.router.navigate(['/new-test-case']);
+  }
+}
