1880c626cf1092e4f2277e5d6da1bd73d4446dfe
TerhesBalazs <terhes.balazs@tigra.hu>
2025-05-24 14:35:17 +0200

    import fix

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DummyHomeComponent } from './dummy-home/dummy-home.component';
+import { TestCaseListComponent } from './test-case-list/test-case-list.component';
+import { NewTestCaseComponent } from './new-test-case/new-test-case.component';
+import { TestReportComponent } from './test-report/test-report.component';
 import { AppRoutingModule } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DummyHomeComponent
+    DummyHomeComponent,
+    TestCaseListComponent,
+    NewTestCaseComponent,
+    TestReportComponent
   ],
   imports: [
     BrowserModule,
