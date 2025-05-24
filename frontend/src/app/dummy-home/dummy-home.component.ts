8047b71afb058d5c811f91d4286d5394cde7d656
TerhesBalazs <terhes.balazs@tigra.hu>
2025-05-24 14:06:55 +0200

    standalone opciók törlése

--- frontend/src/app/dummy-home/dummy-home.component.ts
+++ frontend/src/app/dummy-home/dummy-home.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dummy-home',
-  template: '<h2>Sikeres bejelentkezés!</h2>',
-  standalone: false
+  template: '<h2>Sikeres bejelentkezés!</h2>'
 })
 export class DummyHomeComponent {}
